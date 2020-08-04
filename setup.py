a1480e56ad46c778fb67e3fcbc8c601fd5d408a2
Andy Tran <xpandalord@gmail.com>
2020-08-04 17:22:34 -0400

    updates

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md", "r") as file:
 
 setuptools.setup(
     name="lambdata-xpandalord",
-    version="0.1.0",
+    version="0.1.1",
     author="xpandalord",
     description="A collection of Data Science helper functions",
     long_description=LONG_DESCRIPTION,
