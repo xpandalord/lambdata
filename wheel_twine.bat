10359504410b2fd8b64968cf698c9c212a1cb6e8
Andy Tran <xpandalord@gmail.com>
2020-08-05 12:16:14 -0400

    updates

--- wheel_twine.bat
+++ wheel_twine.bat
@@ -1,3 +1,3 @@
-del build
+del /s /q build, dist, lambdata_xpandalord.egg-info
 python setup.py sdist bdist_wheel
 twine upload -u xpandalord -p #Nyancat19 --repository-url https://test.pypi.org/legacy/ dist/*
